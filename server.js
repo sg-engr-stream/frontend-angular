866091b7c3179b6880a445daf2091c9bc079b51e
Nitin Kumar <nitinkumar6912@gmail.com>
2020-09-13 12:49:20 +0530

    CI/CD

--- server.js
+++ server.js
@@ -13,4 +13,4 @@ app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/frontend-app/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, '0.0.0.0');
